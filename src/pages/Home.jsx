8553b115609f6f276e302a9705a629ae55515ec7
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-08-28 23:56:47 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,10 @@ function Home() {
         <div className="min-h-screen bg-background overflow-x-hidden">
             <Particles
               particleColors={['#717798', '#717798']}
-              particleCount={90}
+              particleCount={700}
               particleSpread={10}
               speed={0.1}
-              particleBaseSize={100}
+              particleBaseSize={35}
               moveParticlesOnHover={true}
               alphaParticles={false}
               disableRotation={false}
