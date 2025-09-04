ea1536bc099d8a4ed06ab74b0c62b173191e0b18
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-09-04 21:48:50 +0200

    Update toaster.jsx

--- src/popup/toaster.jsx
+++ src/popup/toaster.jsx
@@ -6,7 +6,7 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/popup/toast";
+} from "@/popup/toast";
 
 export function Toaster() {
   const { toasts } = useToast();
