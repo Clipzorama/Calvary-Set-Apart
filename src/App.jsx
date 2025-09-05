20181a0afd1ce541c29cb5afd78fada864fd3d95
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-09-06 01:18:37 +0200

    particles everywhere

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import { GetInvolved } from "@/pages/GetInvolved";
 import { Explore } from "@/pages/Explore";
 import { NotFound } from "@/pages/NotFound";
 import { NavBar } from "@/components/Navbar";
-import { Footer } from "@/components/Footer"
-import { Toaster } from "@/popup/toaster"
+import { Footer } from "@/components/Footer";
+import { Toaster } from "@/popup/toaster";
+import Particles  from "@/Effects/Particles";
 
 
 
@@ -15,16 +16,28 @@ import { Toaster } from "@/popup/toaster"
 export default function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/get-involved" element={<GetInvolved />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      <Footer />
-      <Toaster />
+      <div className="min-h-screen bg-background overflow-x-hidden">
+        <Particles
+          particleColors={['#717798', '#717798']}
+          particleCount={700}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={35}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+        <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/get-involved" element={<GetInvolved />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        <Footer />
+        <Toaster />
+      </div>
     </BrowserRouter>
   );
 }
