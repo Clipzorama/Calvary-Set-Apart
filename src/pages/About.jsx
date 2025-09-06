f579479e0357b938286f15dac6508182869b9676
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-09-06 22:38:26 +0200

    added belief section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,13 @@
 import { AboutTop } from "@/aboutparts/AboutTop";
+import { Who } from "@/aboutparts/Who";
+import { Beliefs } from "@/aboutparts/Beliefs";
+
 export const About = () => { 
-    
     return (
         <>
             <AboutTop />  
+            <Who />
+            <Beliefs />
         </>
 
     );
