5d073092b3a6773ec707e5f5734931c894d8688a
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-09-12 05:38:56 +0200

    Update Pillars.jsx

--- src/homeparts/Pillars.jsx
+++ src/homeparts/Pillars.jsx
@@ -1,6 +1,12 @@
 import { Sun, BookOpen, HeartHandshake } from "lucide-react";
 import { Link } from "react-router-dom";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
 
+
+gsap.registerPlugin(ScrollTrigger);
 
 
 const pointers = [
@@ -23,19 +29,68 @@ const pointers = [
 
 
 export const Pillars = () => {
+    const headingRef = useRef(null);
+    const paraRef = useRef(null);
+
+
+
+    useGSAP(() => {
+        gsap.from(headingRef.current, {
+                opacity: 0,
+                x: 100,
+                duration: 1,
+                scrollTrigger: {
+                    trigger: headingRef.current,
+                    start: "top 80%",
+                    end: "top 35%",
+                    toggleActions: "play none none none",
+                    scrub: true,
+                    once: true,
+                },
+        });
+
+        gsap.from(paraRef.current, {
+                opacity: 0,
+                x: 100,
+                duration: 1,
+                scrollTrigger: {
+                    trigger: paraRef.current,
+                    start: "top 80%",
+                    end: "top 35%",
+                    toggleActions: "play none none none",
+                    scrub: true,
+                    once: true,
+                },
+        });
+
+        gsap.from(".pillars", {
+            opacity: 0,
+            duration: 1,
+            scrollTrigger: {
+                trigger: ".pillars",
+                start: "top 80%",
+                end: "top 45%",
+                toggleActions: "play none none none",
+                scrub: true,
+                once: true,
+            },
+            stagger: 0.5,
+            ease: "power2.inOut",
+        });
+        }, {dependencies: []});
     return (
         <section id="pillars" className="py-16 md:py-20 lg:py-30 bg-background">
             <div className="px-10 md:px-20 lg:px-25 xl:px-30 2xl:px-50 flex flex-col items-center ">
-                <h1 className="font-quint font-semibold text-3xl md:text-4xl xl:text-5xl leading-tight md:leading-normal tracking-normal text-primary mb-6 text-center">
+                <h1 ref={headingRef} className="font-quint font-semibold text-3xl md:text-4xl xl:text-5xl leading-tight md:leading-normal tracking-normal text-primary mb-6 text-center">
                     Building Blocks of Our Ministry
                 </h1>
-                <p className="font-poppins leading-6 xs:text-xs sm:text-sm md:text-md xl:text-[1rem] text-foreground/90 max-w-3xl text-center mb-10">
+                <p ref={paraRef} className="font-poppins leading-6 xs:text-xs sm:text-sm md:text-md xl:text-[1rem] text-foreground/90 max-w-3xl text-center mb-10">
                     At our church, we are committed to nurturing a vibrant community through worship, teaching, and outreach. These pillars guide our mission and strengthen our faith.    
                 </p>
 
                 <div className="flex flex-col lg:flex-row gap-12 text-center mb-17">
                     {pointers.map((item, index) => (
-                        <div key={index} className="flex flex-col items-center px-6 gap-3">
+                        <div key={index} className="flex flex-col items-center px-6 gap-3 pillars">
                             {item.icon}
                             <h3 className="text-xl font-bold">{item.title}</h3>
                             <p className="font-poppins text-sm leading-6 text-foreground/90">{item.text}</p>
